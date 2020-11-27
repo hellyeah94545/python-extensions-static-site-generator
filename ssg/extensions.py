4475a2d078dde329249d11fee694fb102aa9dccf
hellyeah94545 <73657016+hellyeah94545@users.noreply.github.com>
2020-11-27 00:03:52 -0800

    ˚module 1

--- ssg/extensions.py
+++ ssg/extensions.py
@@ -1,4 +1,24 @@
 import sys
 import importlib
-from pathlib import path
+from pathlib import Path
 
+
+def load_module(directory, name):
+    """
+
+    :param directory: location of file
+    :param name: file name
+    :return: module object
+    """
+    sys.path.insert(0, directory)
+    importlib.import_module(name)
+    sys.path.pop(0)
+
+def load_directory(directory):
+    """
+
+    :param directory: Extention directory
+    :return:
+    """
+    for path in directory.rglob("*.py"):
+            load_module(directory.as_posix(), path.stem)
