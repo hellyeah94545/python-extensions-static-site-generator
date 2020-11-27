5ff263e7099173f4e90545c3ab6c49462d0e7e2e
hellyeah94545 <73657016+hellyeah94545@users.noreply.github.com>
2020-11-27 00:10:53 -0800

    ˚module 1

--- ssg/extensions.py
+++ ssg/extensions.py
@@ -22,3 +22,12 @@ def load_directory(directory):
     """
     for path in directory.rglob("*.py"):
             load_module(directory.as_posix(), path.stem)
+
+def load_bundled():
+    """
+
+    :return:
+    """
+    directory = Path(__file__).parent / "extensions"
+    load_directory(directory)
+
